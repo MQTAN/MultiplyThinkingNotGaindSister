677e8c752da878569b1b03e52d926eb092f2226c
tanmanqi <tanmanqi@MQPro.local>
2015-09-15 11:58:38 +0800

    新建了一个项目

---
+++ MultiplyThinkingForSister/MultiplyThinkingForSister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiplyThinkingForSister
+//
+//  Created by tanmanqi on 15/9/15.
+//  Copyright (c) 2015年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiplyThinkingForSister/MultiplyThinkingForSister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultiplyThinkingForSister
+//
+//  Created by tanmanqi on 15/9/15.
+//  Copyright (c) 2015年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
